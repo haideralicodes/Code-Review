aefe3879f920cd46ca89a1ab7f8303a47a4e71fc
Haider Ali <haiderali920687@gmail.com>
2025-02-12 19:33:13 +0500

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const app = require('./src/app')
 
+app.get('/', (req, res)=>{
+    res.send('Hello World') 
+}) 
 
 app.listen(3000, ()=>{
     console.log("server running on port 3000")
